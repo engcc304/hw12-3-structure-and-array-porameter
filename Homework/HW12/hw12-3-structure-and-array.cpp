d70a0f9572977eafb33af69b1eb856ae0091deb3
porameter <paramet_su66@live.rmutl.ac.th>
2023-10-11 23:17:00 +0700

    update hw12-3

--- Homework/HW12/hw12-3-structure-and-array.cpp
+++ Homework/HW12/hw12-3-structure-and-array.cpp
@@ -42,3 +42,53 @@
 */
 
 //TODO: #21 output money should in decimal format (eg. 120,200,340.42).
+
+#include <stdio.h>
+#include <string.h>
+
+struct Employee {
+    char Name[50] ;
+    double Salary ;
+    int Duration ;
+};
+
+int main() {
+    int n ;
+    double totalSalary = 0 ;
+    double maxDuration = 0 ;
+    struct Employee maxDurationEmployee ;
+
+    printf( "Do you want to Enter Employee Information? (y/n) : " ) ;
+    char choice ;
+    scanf( " %c" , &choice ) ;
+
+    while ( choice == 'y' || choice == 'Y' ) {
+        struct Employee emp ;
+
+        printf( "----\n" ) ;
+        printf( "Employee Name : " ) ;
+        scanf( "%s" , emp.Name ) ;
+        printf( "Salary (Bath/Month) : " ) ;
+        scanf( "%lf" , &emp.Salary ) ;
+        printf( "Duration (Year) : " ) ;
+        scanf( "%d" , &emp.Duration ) ;
+
+        totalSalary += emp.Salary ;
+        if ( emp.Duration > maxDuration ) {
+            maxDuration = emp.Duration ;
+            maxDurationEmployee = emp; 
+        }
+
+        printf( "Do you want to Enter Employee Information? (y/n) : " ) ;
+        scanf( " %c" , &choice ) ;
+    }
+
+    printf( "Average of Salary : %.2lf Bath\n", totalSalary / n ) ;
+    printf( "Payment of every month : %.2lf Bath\n" , totalSalary * 12 ) ;
+    printf( "----------------------------------------\n" ) ;
+    printf( "** Most duration in this business **\n" ) ;
+    printf( "Name : %s (%d Years)\n" , maxDurationEmployee.Name , maxDurationEmployee.Duration ) ;
+    printf( "Salary : %.2lf Bath\n" , maxDurationEmployee.Salary ) ;
+
+    return 0 ;
+}
